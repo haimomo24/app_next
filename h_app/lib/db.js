a07eb2a18594b360ab156f223178c00131d9f56b
haimomo24 <135099315+haimomo24@users.noreply.github.com>
2024-09-19 00:04:39 +0700

    push

---
+++ h_app/lib/db.js
@@ -0,0 +1,6 @@
+const pool = mysql.createPool({  
+    host: 'localhost',  
+    user: 'root', // Thay thế với người dùng của bạn  
+    password: '', // Thay thế với mật khẩu của bạn  
+    database: 'hhstore' // Tên cơ sở dữ liệu  
+  });  
